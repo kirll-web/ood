8612c0b49008fa073c853d636f69ed4a77b56c5e
kirll-web <reghansan@gmail.com>
2024-10-13 11:15:09 +0300

    [lab_3-1] Выполнил первое задание

--- lab3/src/main/kotlin/Beverages.kt
+++ lab3/src/main/kotlin/Beverages.kt
@@ -16,22 +16,74 @@ open class Coffee(description: String = "Coffee") : Beverage(description) {
     override fun getCost(): Double = 60.0
 }
 
-// Капуччино
-class Cappuccino : Coffee("Cappuccino") {
-    override fun getCost(): Double = 80.0
-}
-
-// Латте
-class Latte : Coffee("Latte") {
-    override fun getCost() = 90.0
-}
-
-// Чай
-class Tea : Beverage("Tea") {
+open class Tea : Beverage("Tea") {
     override fun getCost() = 30.0
 }
 
-// Молочный коктейль
-class Milkshake : Beverage("Milkshake") {
-    override fun getCost() = 80.0
+class MilkUlunTea : Tea() {
+    override fun getDescription() = "Milk Ulun ${super.getDescription()}"
 }
+
+class ShuPuerTea : Tea() {
+    override fun getDescription() = "ShuPuer ${super.getDescription()}"
+}
+
+class BubbleTea : Tea() {
+    override fun getDescription() = "Bubble ${super.getDescription()}"
+}
+
+class MangoTea : Tea() {
+    override fun getDescription() = "Mango  ${super.getDescription()}"
+}
+
+// Капуччино
+open class Cappuccino : Coffee("Cappuccino") {
+    override fun getCost(): Double = 80.0
+}
+
+class DoubleCappuccino : Cappuccino() {
+    override fun getDescription() = "Double ${super.getDescription()}"
+    override fun getCost(): Double = 120.0
+}
+
+// Латте
+open class Latte : Coffee("Latte") {
+    override fun getCost() = 90.0
+}
+
+class DoubleLatte : Latte() {
+    override fun getDescription() = "Double ${super.getDescription()}"
+
+    override fun getCost() = 130.0
+}
+
+// Молочный коктейль
+class Milkshake(private val mSize: MilkshakeSize) : Beverage("Milkshake") {
+    override fun getCost() = mSize.getCost()
+    override fun getDescription(): String {
+        val stringRes = when (mSize) {
+            MilkshakeSize.SMALL -> "Small"
+            MilkshakeSize.MEDIUM -> "Medium"
+            MilkshakeSize.LARGE -> "Large"
+        }
+        return  "$stringRes ${super.getDescription()}"
+    }
+}
+
+enum class MilkshakeSize(private val value: UInt) {
+    SMALL(0U),
+    MEDIUM(1U),
+    LARGE(2U);
+
+    fun getCost() = when (value) {
+        SMALL.value -> 50.0
+        MEDIUM.value -> 50.0
+        LARGE.value -> 50.0
+        else -> throw IllegalArgumentException("Incorrect size")
+    }
+}
+
+/* todo Ввести стандартную и двойную порцию латте. Двойная порция стоит 130 рублей, стандартная – 90.
+    Ввести стандартную (80) и двойную (120р) порции капучино
+    Предлагать покупателям 4 сорта чая (цена не зависит от сорта чая). Названия сортов выберите на свое усмотрение.
+    Предлагать маленькую (50 р), среднюю (60р) и большую (80р) порции молочных коктейлей*/
--- lab3/src/main/kotlin/Main.kt
+++ lab3/src/main/kotlin/Main.kt
@@ -1,5 +1,6 @@
 package org.example
 
+import java.util.UUID
 import kotlin.reflect.full.primaryConstructor
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -21,11 +22,107 @@ fun makeCinnamon(): (b: IBeverage) -> Cinnamon = { b ->
 }
 
 
-inline fun <reified T : Any> makeCondiment(vararg params: Any?): (IBeverage) -> T = { base ->
+fun getAmount(name: String): UInt {
+    var amount: UInt
+    while (true) {
+        println("Amount $name: ")
+        try {
+            amount = readln().toUInt()
+            break
+        } catch (ex: Exception) {
+            println("Incorrect input")
+            continue
+        }
+    }
+    return amount
+}
+
+fun getIceCubesType(): IceCubeType {
+    var number: UInt
+    var type: IceCubeType
+    while (true) {
+        println("Ice Cubes Type: 1 - Dry, 2 - Water ")
+        try {
+            number = readln().toUInt()
+
+            type = when (number) {
+                1U -> IceCubeType.Dry
+                2U -> IceCubeType.Water
+                else -> {
+                    println("Incorrect input")
+                    continue
+                }
+            }
+
+            break
+        } catch (ex: Exception) {
+            println("Incorrect input")
+            continue
+        }
+    }
+    return type
+}
+
+
+fun getSyrupType(): SyrupType {
+    var number: UInt
+    var type: SyrupType
+    while (true) {
+        println("Syrup Type: 1 - Chocolate, 2 - Maple ")
+        try {
+            number = readln().toUInt()
+
+            type = when (number) {
+                1U -> SyrupType.Chocolate
+                2U -> SyrupType.Maple
+                else -> {
+                    println("Incorrect input")
+                    continue
+                }
+            }
+
+            break
+        } catch (ex: Exception) {
+            println("Incorrect input")
+            continue
+        }
+    }
+    return type
+}
+
+fun getMilkshakeSize(): MilkshakeSize {
+    var number: UInt
+    var size: MilkshakeSize
+    while (true) {
+        println(" Milkshake size: 1 - Small, 2 - Medium, 3 - Large")
+        try {
+            number = readln().toUInt()
+
+            size = when (number) {
+                1U -> MilkshakeSize.SMALL
+                2U ->MilkshakeSize.MEDIUM
+                3U ->MilkshakeSize.LARGE
+
+                else -> {
+                    println("Incorrect input")
+                    continue
+                }
+            }
+
+            break
+        } catch (ex: Exception) {
+            println("Incorrect input")
+            continue
+        }
+    }
+    return size
+}
+
+inline fun <reified T : Any> makeCondiment(vararg params: Any?): (b: IBeverage) -> T = { b ->
     val constructor =
         T::class.primaryConstructor ?: throw IllegalArgumentException("Нет доступного конструктора")
 
-    constructor.call(base, *params)  // Создаём экземпляр, передавая аргументы
+    constructor.call(b, *params)  // Создаём экземпляр, передавая аргументы
 }
 
 
@@ -34,32 +131,68 @@ operator fun <Component, Decorator> Component.plus(decorator: (Component) -> Dec
 }
 
 fun dialogWithUser() {
-    println("Type 1 for coffee or 2 for tea")
-    var beverageChoice = 0
-    beverageChoice = readln().toInt()
-
-    lateinit var beverage: IBeverage
-
-    if (beverageChoice == 1) {
-        beverage = Coffee()
-    } else if (beverageChoice == 2) {
-        beverage = Tea()
-    } else {
-        return
-    }
-    var condimentChoice: Int = 0
+    println("Type 1 for coffee, " +
+            "2 for tea, " +
+            "3 for Cappuccino, " +
+            "4 for Latte, " +
+            "5 for Milkshake," +
+            " 6 - Double Cappuccino," +
+            " 7 - Double Latte" +
+            " 8 - ShuPuer Tea" +
+            " 9 - Milk Ulun Tea" +
+            " 10 - Bubble Tea" +
+            " 11 - Mango Tea")
+    var beverageChoice: UInt
+    var beverage: IBeverage
     while (true) {
-        println("1 - Lemon, 2 - Cinnamon, 0 - Checkout")
-        condimentChoice = readln().toInt()
+        try {
+            beverageChoice = readln().toUInt()
+            beverage = when (beverageChoice) {
+                1U -> Coffee()
+                2U -> Tea()
+                3U -> Cappuccino()
+                4U -> Latte()
+                5U -> Milkshake(getMilkshakeSize())
+                6U -> DoubleCappuccino()
+                7U -> DoubleLatte()
+                8U -> ShuPuerTea()
+                9U -> MilkUlunTea()
+                10U -> BubbleTea()
+                11U -> MangoTea()
 
-        if (condimentChoice == 1) {
-            beverage += makeLemon(2U)
-        } else if (condimentChoice == 2) {
-            beverage += makeCinnamon()
-        } else if (condimentChoice == 0) {
+                else -> {
+                    println("Incorrect input")
+                    continue
+                }
+            }
             break
-        } else {
-            return
+        } catch (ex: Exception) {
+            println("Incorrect input")
+            continue
+        }
+    }
+
+    var condimentChoice: Int
+    while (true) {
+        println("1 - Lemon, 2 - Cinnamon, 3 - IceCubes, 4 - ChocolateCrumbs, 5 - CoconutFlakes, 6 - Syrup, 0 - Checkout")
+
+        try {
+            condimentChoice = readln().toInt()
+        } catch (ex: Exception) {
+            println("Incorrect input")
+            continue
+        }
+
+
+        when (condimentChoice) {
+            1 -> beverage += makeLemon(getAmount("lemon"))
+            2 -> beverage += makeCinnamon()
+            3 -> beverage += makeCondiment<IceCubes>(getAmount("IceCubes"), getIceCubesType())
+            4 -> beverage += makeCondiment<ChocolateCrumbs>(getAmount("ChocolateCrumbs"))
+            5 -> beverage += makeCondiment<CoconutFlakes>(getAmount("CoconutFlakes"))
+            6 -> beverage += makeCondiment<Syrup>(getSyrupType())
+
+            0 -> break
         }
     }
 
@@ -70,53 +203,4 @@ fun dialogWithUser() {
 fun main() {
     dialogWithUser()
     println()
-
-    val latte = Latte()
-    // добавляем корицы
-    val cinnamon = Cinnamon((latte))
-    // добавляем пару долек лимона
-    val lemon = Lemon(cinnamon, 2U)
-    // добавляем пару кубиков льда
-    val iceCubes = IceCubes(beverage = lemon, quantity = 2U, type = IceCubeType.Dry)
-    // добавляем 2 грамма шоколадной крошки
-    val beverage1 = ChocolateCrumbs(iceCubes, 2U)
-
-    // Выписываем счет покупателю
-    println("${beverage1.getDescription()} , cost: ${beverage1.getCost()}")
-
-    val beverage2 = ChocolateCrumbs(
-        IceCubes(
-            Lemon(Cinnamon(Latte()), 2U), 2U, IceCubeType.Dry
-        ),
-        2U
-    )
-
-    // Выписываем счет покупателю
-    println("${beverage2.getDescription()} , cost: ${beverage2.getCost()}")
-
-    // Подробнее рассмотрим работу MakeCondiment и оператора <<
-    val lemon2 = makeCondiment<Lemon>(2U)
-    // iceCubes - функция, добавляющая "3 кусочка льда" к любому напитку
-    val iceCubes3 = makeCondiment<IceCubes>(3U, IceCubeType.Water)
-
-    val tea = Tea()
-
-    // декорируем чай двумя дольками лимона и тремя кусочками льда
-    val lemonIceTea = iceCubes3(lemon2(tea))
-    println("${lemonIceTea.getDescription()} , cost: ${lemonIceTea.getCost()}")
-    val oneMoreLemonIceTea = Tea() +
-            makeCondiment<Lemon>(2U) +
-            makeCondiment<IceCubes>(3U, IceCubeType.Water)
-    println("${oneMoreLemonIceTea.getDescription()} , cost: ${oneMoreLemonIceTea.getCost()}")
-
-    val beverage3 = Latte() + makeCondiment<Cinnamon>() + makeCondiment<Lemon>(2U) +
-            makeCondiment<IceCubes>(2U, IceCubeType.Dry)
-    println("${beverage3.getDescription()} , cost: ${beverage3.getCost()}")
-
-    val beverage4 = Milkshake() +
-            makeCondiment<Syrup>(SyrupType.Maple) +
-            makeCondiment<CoconutFlakes>(8U)
-
-    // Выписываем счет покупателю
-    println("${beverage4.getDescription()} , cost: ${beverage4.getCost()}")
 }
